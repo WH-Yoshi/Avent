bf309b3fb63344637bbfa199c15683df38713b42
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-11-22 14:48:51 +0100

    Routes are done

--- APIF1/src/App.jsx
+++ APIF1/src/App.jsx
@@ -1,36 +1,42 @@
-import { Layout, List } from "antd";
-import { Content } from "antd/es/layout/layout";
-import axios from "axios"
-import { useState } from "react"
-import { useEffect } from "react"
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Root from '../pages/Root';
+import Seasons from '../pages/Seasons';
+import Drivers from '../pages/Drivers';
+import Main from '../pages/Main';
 
 function App() {
-  const [drivers, setDrivers] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("https://ergast.com/api/f1/2020/drivers.json")
-      .then((response) => {
-        setDrivers(response.data.MRData.DriverTable.Drivers);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root/>,
+      children:[
+        {
+          path:'Seasons',
+          element:<Seasons/>
+        },
+        {
+          path: 'Drivers',
+          element: <Drivers/>
+        },
+        {
+          path: 'Main',
+          element: <Main/>
+        },
+        {
+          path: '*',
+          element: <h1>404</h1>
+        }
+      ]
+    }
+  ])
 
   return (
-    <Layout>
-      <Content>
-        <List
-          bordered
-          dataSource={drivers}
-          renderItem={(driver) => (
-            <List.Item>{driver.givenName + " " + driver.familyName}</List.Item>
-          )}
-        />
-      </Content>
-    </Layout>
-  );
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
 }
-  
+
 export default App
