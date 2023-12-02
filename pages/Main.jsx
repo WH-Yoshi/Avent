2ae412c08ff16ee06d095adcf2c718f311383bc6
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-12-02 18:22:27 +0100

    Delete pages directory

--- pages/Main.jsx
+++
@@ -1,7 +0,0 @@
-export default function Main(props) {
-  return(
-    <>
-        <h1>Page Main</h1>
-    </>
-  )
-}
--- pages/Seasons.jsx
+++
@@ -1,8 +0,0 @@
-export default function Seasons (props) {
-
-    return(
-        <>
-            <h1>Page seasons</h1>
-        </>
-    )
-}
