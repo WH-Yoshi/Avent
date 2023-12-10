02cd72d43b1387fb052b361f602859eb28ad3de2
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-12-10 21:44:52 +0100

    historical done

--- src/pages/Historic.jsx
+++ src/pages/Historic.jsx
@@ -9,7 +9,7 @@ export default function Historic (props) {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'http://ergast.com/api/f1/1988/11/results.json'
+          'http://ergast.com/api/f1/1988/11/results.json?limit=40'
         );
         const results = response.data.MRData.RaceTable.Races[0].Results;
         setResults(results);
@@ -20,6 +20,14 @@ export default function Historic (props) {
 
     fetchData();
   }, []);
+
+  const displayTimeOrStatus = (result) => {
+    if (result.Time && result.Time.time) {
+      return <h4 className='stats'>{result.Time.time}</h4>;
+    } else {
+      return <h4 className='stats'>{result.status}</h4>;
+    }
+  };
 
   return (
     <>
@@ -32,15 +40,28 @@ export default function Historic (props) {
         </div>
         <div className='results'>
           <div className="table">
-            {/* {results.map((result, index) => (
-              <div key={index}>
-                <h2>{result.Driver.givenName} {result.Driver.familyName}</h2>
-                <p>Position: {result.position}</p>
-                <p>Points: {result.points}</p>
+            <div className='columname'>
+              <h3 className='pos'>POS</h3>
+              <h3 className='num'>N°</h3>
+              <h3 className='name'>DRIVER</h3>
+              <h3 className='car'>CAR</h3>
+              <h3 className='laps'>LAPS</h3>
+              <h3 className='stats'>TIME/RETIRED</h3>
+              <h3 className='pts'>PTS</h3>
+            </div>
+            {results.map((result, index) => (
+              <div key={index} className={`driveresult ${index % 2 === 0 ? 'gray' : ''}`}>
+                <h4 className='pos'>{result.position}</h4>
+                <h4 className='num'>{result.number}</h4>
+                <h4 className='name'>{result.Driver.givenName} {result.Driver.familyName}</h4>
+                <h4 className='car'>{result.Constructor.name}</h4>
+                <h4 className='laps'>{result.laps}</h4>
+                {displayTimeOrStatus(result)}
+                <h4 className='pts'>{result.points}</h4>
               </div>
-            ))} */}
+            ))}
           </div>
         </div>
     </>
-  )
+  );
 }
