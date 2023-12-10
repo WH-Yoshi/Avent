3307bd76da5af72b964670c5fa97e9d860425694
WH-Yoshi <130357229+WH-Yoshi@users.noreply.github.com>
2023-12-10 16:14:14 +0100

    start of historical page

--- src/pages/Historic.jsx
+++ src/pages/Historic.jsx
@@ -3,17 +3,16 @@ import axios from 'axios';
 
 export default function Historic (props) {
 
-  const [constructorData, setConstructorData] = useState([]);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'http://ergast.com/api/f1/2023/constructorstandings.json'
+          'http://ergast.com/api/f1/1988/11/results.json'
         );
-        const constructors = response.data.MRData.StandingsTable.StandingsLists[0]
-          .ConstructorStandings.slice(0, 10);
-        setConstructorData(constructors);
+        const results = response.data.MRData.RaceTable.Races[0].Results;
+        setResults(results);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -26,8 +25,22 @@ export default function Historic (props) {
     <>
         <div className='historicontainer'>
             <div className='presentation'>
-                <h1>Grand Prix</h1>
+
             </div>
+        </div>
+        <div className='results'>
+          <div className='title'>
+            <h1>1993 FORMULA ONE<br/>Belgian Grand Prix</h1>
+          </div>
+          <div className="table">
+            {/* {results.map((result, index) => (
+              <div key={index}>
+                <h2>{result.Driver.givenName} {result.Driver.familyName}</h2>
+                <p>Position: {result.position}</p>
+                <p>Points: {result.points}</p>
+              </div>
+            ))} */}
+          </div>
         </div>
     </>
   )
